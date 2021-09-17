510625bb52fcdb6a42a40a13cdb9125130072d8e
jofurdz <jofurdz@gmail.com>
2021-09-17 10:09:58 -0500

    task 0 and 2

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * 
+ * 
+ * 
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t x, j, blah;
+
+	for (x = 0; x < size; x++)
+	{
+		for (j= 0; j < size - x - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				blah = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = blah;
+				print_array(array, size);
+			}
+		}
+	}
+}
